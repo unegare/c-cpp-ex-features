1d6bfba3543e08e47f6d3501537e7d9eb255a5d6
unegare <dronte2008@yandex.ru>
2018-05-03 13:24:29 +0300

    ex_002_adl upd

--- ex_002_adl.cpp
+++ ex_002_adl.cpp
@@ -3,6 +3,14 @@
 
 //WITHOUT namespace claiming !
 
+namespace my {
+  class data {
+    public:
+    uint32_t a;
+  };
+  void set (class data& d, uint32_t val);
+};
+
 int main () {
   operator<<(std::cout, "Test");
   endl(std::cout);
@@ -12,5 +20,15 @@ int main () {
     operator<<(std::cout, std::to_string(*it));
   }
   endl(std::cout);
+
+  class my::data d;
+
+  set(d, 10);
+
   return 0;
+}
+
+void my::set (class my::data& d, uint32_t val) {
+  d.a = val;
+  std::cout << "I'm" << std::endl;
 }
