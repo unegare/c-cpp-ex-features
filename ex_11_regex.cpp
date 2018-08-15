187723b0c8442a519de6a4394532d45268aefae7
unegare <dronte2008@yandex.ru>
2018-08-15 15:07:01 +0300

    ex_11_regex removing escape before dot(.)

--- ex_11_regex.cpp
+++ ex_11_regex.cpp
@@ -4,7 +4,7 @@
 int main () {
   const char *row = R"(function (uint, uint, uint[], struct Temp.data storage pointer) public returns (uint) )";
   try {
-    std::regex_token_iterator<const char *>::regex_type re(R"(\([a-zA-Z0-9\[\],\. ]+\))");
+    std::regex_token_iterator<const char *>::regex_type re(R"(\([a-zA-Z0-9\[\],. ]+\))");
     std::regex_token_iterator<const char *> next(row, row + strlen(row), re);
     std::regex_token_iterator<const char*> end;
     
