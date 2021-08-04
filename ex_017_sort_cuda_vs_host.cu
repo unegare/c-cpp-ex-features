17bdcce586d5347de1581577c8a6cc34611dfbc6
unegare <dronte2008@yandex.ru>
2021-08-04 09:57:37 +0300

    ex_017 sort cuda vs host

---
+++ ex_017_sort_cuda_vs_host.cu
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+
+int main() {
+  std::ios::sync_with_stdio(false);
+
+  //32M random values
+  thrust::host_vector<int> h_vec(32 << 20);
+  thrust::generate(begin(h_vec), end(h_vec), rand);
+  
+  auto t1 = std::chrono::high_resolution_clock::now();
+
+  thrust::device_vector<int> d_vec = h_vec;
+  thrust::sort(d_vec.begin(), d_vec.end());
+  thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+
+  auto t2 = std::chrono::high_resolution_clock::now();
+
+  //Duration (cuda): 676ms (GTX 1080 TI, driver 470, cuda 11.4)
+  std::cout << "Duration (cuda): " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms" << std::endl;
+
+
+
+  std::vector<int> v(h_vec.begin(), h_vec.end());
+
+  auto t3 = std::chrono::high_resolution_clock::now();
+
+  std::sort(begin(v), end(v));
+
+  auto t4 = std::chrono::high_resolution_clock::now();
+
+  //Duration (host): 6919ms (i3 8350K @ 4.00 GHz, cache 8MB, DDR4 8Gb x2 3000 MHz)
+  std::cout << "Duration (host): " << std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count() << "ms" << std::endl;
+
+  return 0;
+}
